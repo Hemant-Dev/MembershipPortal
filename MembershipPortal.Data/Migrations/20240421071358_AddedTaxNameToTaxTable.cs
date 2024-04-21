4f5cd6b65dde0d27479b517d0202f534b8f544c4
Hemant-Dev <phemant092@gmail.com>
2024-04-21 12:50:39 +0530

    added new Migration for Tax Name

---
+++ MembershipPortal.Data/Migrations/20240421071358_AddedTaxNameToTaxTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MembershipPortal.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTaxNameToTaxTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TaxName",
+                table: "Taxes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaxName",
+                table: "Taxes");
+        }
+    }
+}
